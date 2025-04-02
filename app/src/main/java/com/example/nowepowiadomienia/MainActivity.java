5f60b16a0b6f9f68e416912f9c5b35c2ebb3cf52
LordOwing <michallolo12345@gmail.com>
2025-04-02 08:10:54 +0200

    cos

--- app/src/main/java/com/example/nowepowiadomienia/MainActivity.java
+++ app/src/main/java/com/example/nowepowiadomienia/MainActivity.java
@@ -1,24 +1,82 @@
 package com.example.nowepowiadomienia;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    static int NOTIFICATION_ID = 0;
+    void wyslijPowiadomienie(String title, String message, String CHANNEL_NAME, String CHANNEL_ID, int obrazek,int style){
+        Bitmap bitmap;
+        bitmap = BitmapFactory.decodeResource(this.getResources(), obrazek);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            CharSequence name = CHANNEL_NAME;
+            String description = "Opis Kanału Powiadomień";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notifiactionManager = this.getSystemService(NotificationManager.class);
+            notifiactionManager.createNotificationChannel(channel);
+        }
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if(this.checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS)
+                    != PackageManager.PERMISSION_GRANTED){
+                this.requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
+                return;
+            }
+        }
+        Intent Intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, Intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+
+
+                .setContentText(message)
+
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        switch (style){
+            case 1:
+                builder
+                        .setContentTitle(title + NOTIFICATION_ID)
+                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap));
+
+        }
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+        NOTIFICATION_ID++;
+
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button guzik1= findViewById(R.id.guzik1);
+        guzik1.setOnClickListener(v->{
+            wyslijPowiadomienie("Tytuł powiadomienia", "Hejka kociaczki", "CHANNEL", "1", 0, 1);
         });
+
     }
 }
